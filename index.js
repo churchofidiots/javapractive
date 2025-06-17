67eddf23f8b7078d43775decfae7af16a2398b01
churchofidiots <wolfnamiko@gmail.com>
2025-06-17 03:47:11 +0000

    java

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 // Comments here!
  console.log('Hello World');
 
- let name = 'Mosh';
+ let name = 'Nami';
  console.log(name);
+
+ //cannot be a reserved keyword
+ //should be meaningful (to clue the meaning)
+ //cannot start with a number (1name)
+ //cannot contain a space or hyphen
+ //are case sensitve
+ 
+ let firstName = 'Nami';
+ let lastName = 'Pearl';
+
+ let interestRate = 0.3;
+ interestRate = 1;
+ console.log(interestRate);
